1d6318603a1c39bfcc0e8a3a3e730da13fd8e153
carlov20 <carlov20@users.noreply.github.com>
2021-10-22 09:33:44 +0100

    fixed lint

--- server/controllers/allocationsController.ts
+++ server/controllers/allocationsController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express'
 import AllocationsService from '../services/allocationsService'
+import logger from '../../logger'
 
 export default class AllocationsController {
   constructor(private readonly allocationsService: AllocationsService) {}
 
   async getAllocations(req: Request, res: Response): Promise<void> {
     const response = await this.allocationsService.getUnallocatedCases()
-    console.log(response)
+    logger.info(response)
     res.render('pages/index', { data: response })
   }
 }

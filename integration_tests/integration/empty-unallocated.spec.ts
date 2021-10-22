49b724e65a17be7ed595ce055e1910619b0d652e
carlov20 <carlov20@users.noreply.github.com>
2021-10-22 10:53:09 +0100

    WFP-627 removed duplicate tests

--- integration_tests/integration/empty-unallocated.spec.ts
+++ integration_tests/integration/empty-unallocated.spec.ts
@@ -1,5 +1,4 @@
 import UnallocatedPage from '../pages/unallocated'
-import AuthSignInPage from '../pages/authSignIn'
 import Page from '../pages/page'
 
 context('Unallocated', () => {
@@ -10,42 +9,9 @@ context('Unallocated', () => {
     cy.task('stubGetNoAllocations')
   })
 
-  it('User name visible in header', () => {
-    cy.signIn()
-    const unallocatedPage = Page.verifyOnPage(UnallocatedPage)
-    unallocatedPage.headerUserName().should('contain.text', 'J. Smith')
-  })
-
-  it('Probation Delivery Unit visible on page', () => {
-    cy.signIn()
-    const unallocatedPage = Page.verifyOnPage(UnallocatedPage)
-    unallocatedPage.probationDeliveryUnit().should('contain.text', 'Gateshead and South Tyneside')
-  })
-
-  it('Primary nav visible on page', () => {
-    cy.signIn()
-    const unallocatedPage = Page.verifyOnPage(UnallocatedPage)
-    unallocatedPage
-      .primaryNav()
-      .should('contain', 'Allocations')
-      .and('contain', 'Offender Management')
-      .and('contain', 'OMIC')
-      .and('contain', 'Courts')
-      .and('contain', 'Expiring Reductions')
-      .and('contain', 'Search')
-  })
-
   it('Must not show the notifications badge when no unallocated cases exist', () => {
     cy.signIn()
     const unallocatedPage = Page.verifyOnPage(UnallocatedPage)
     unallocatedPage.notificationsBadge().should('not.exist')
-  })
-
-  it('User can log out', () => {
-    cy.signIn()
-    const unallocatedPage = Page.verifyOnPage(UnallocatedPage)
-    unallocatedPage.signOut().click()
-    const authPage = new AuthSignInPage()
-    authPage.checkOnPage()
   })
 })

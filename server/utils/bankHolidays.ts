9dcd11de654a76200f3c4a90319e844e301cabec
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2021-12-02 17:39:08 +0000

    :sparkles: WFP-756: Add sub bar to display when not booked

---
+++ server/utils/bankHolidays.ts
@@ -0,0 +1,27 @@
+import moment from 'moment-business-days'
+import axios from 'axios'
+import logger from '../../logger'
+
+const getBankHolidays = async () => {
+  let response = {}
+  const url = 'https://www.gov.uk/bank-holidays.json'
+  try {
+    response = await axios.get(url)
+  } catch (error) {
+    return error.response
+  }
+  return response
+}
+
+const applyBankHols = async () => {
+  return getBankHolidays().then(data => {
+    const dates = data.data['england-and-wales'].events.map((holiday: any) => holiday.date)
+    moment.updateLocale('en', {
+      holidays: dates,
+      holidayFormat: 'YYYY-MM-DD',
+    })
+    logger.info(`adding bank holidays: ${dates}`)
+  })
+}
+
+export default applyBankHols

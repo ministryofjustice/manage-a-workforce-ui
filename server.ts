2586e37405e9ffa169a14f4eee2073f368fcf1d7
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2022-01-11 15:36:21 +0000

    :arrow_up: upgrade dependencies

--- server.ts
+++ server.ts
@@ -3,14 +3,21 @@
  * Do appinsights first as it does some magic instrumentation work, i.e. it affects other 'require's
  * In particular, applicationinsights automatically collects bunyan logs
  */
+import fs from 'fs'
 import { initialiseAppInsights, buildAppInsightsClient } from './server/utils/azureAppInsights'
 
 initialiseAppInsights()
 buildAppInsightsClient()
 
-import app from './server/index'
+import createApplication from './server/index'
 import logger from './logger'
 
-app.listen(app.get('port'), () => {
-  logger.info(`Server listening on port ${app.get('port')}`)
-})
+createApplication()
+  .then(app => {
+    app.listen(app.get('port'), () => {
+      logger.info(`Server listening on port: ${app.get('port')}`)
+    })
+  })
+  .catch(error => {
+    logger.error(`Failed to start application: ${error.message}`)
+  })
--- server/data/hmppsAuthClient.ts
+++ server/data/hmppsAuthClient.ts
@@ -1,6 +1,33 @@
+import superagent from 'superagent'
+import querystring from 'querystring'
+import type TokenStore from './tokenStore'
+
 import logger from '../../logger'
 import config from '../config'
+import generateOauthClientToken from '../authentication/clientCredentials'
 import RestClient from './restClient'
+
+const timeoutSpec = config.apis.hmppsAuth.timeout
+const hmppsAuthUrl = config.apis.hmppsAuth.url
+
+function getApiClientTokenFromHmppsAuth(username?: string): Promise<superagent.Response> {
+  const clientToken = generateOauthClientToken(config.apis.hmppsAuth.apiClientId, config.apis.hmppsAuth.apiClientSecret)
+
+  const authRequest = username
+    ? querystring.stringify({ grant_type: 'client_credentials', username })
+    : querystring.stringify({ grant_type: 'client_credentials' })
+
+  logger.info(
+    `HMPPS Auth request '${authRequest}' for client id '${config.apis.hmppsAuth.apiClientId}' and user '${username}'`
+  )
+
+  return superagent
+    .post(`${hmppsAuthUrl}/oauth/token`)
+    .set('Authorization', clientToken)
+    .set('content-type', 'application/x-www-form-urlencoded')
+    .send(authRequest)
+    .timeout(timeoutSpec)
+}
 
 export interface User {
   name: string
@@ -12,6 +39,8 @@ export interface UserRole {
 }
 
 export default class HmppsAuthClient {
+  constructor(private readonly tokenStore: TokenStore) {}
+
   private restClient(token: string): RestClient {
     return new RestClient('HMPPS Auth Client', config.apis.hmppsAuth, token)
   }
@@ -25,5 +54,21 @@ export default class HmppsAuthClient {
     return this.restClient(token)
       .get({ path: '/api/user/me/roles' })
       .then(roles => (<UserRole[]>roles).map(role => role.roleCode)) as Promise<string[]>
+  }
+
+  async getApiClientToken(username?: string): Promise<string> {
+    const key = username || '%ANONYMOUS%'
+
+    const token = await this.tokenStore.getToken(key)
+    if (token) {
+      return token
+    }
+
+    const newToken = await getApiClientTokenFromHmppsAuth(username)
+
+    // set TTL slightly less than expiry of token. Async but no need to wait
+    await this.tokenStore.setToken(key, newToken.body.access_token, newToken.body.expires_in - 60)
+
+    return newToken.body.access_token
   }
 }

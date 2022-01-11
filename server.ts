dd4651fbd02790d05de694caba534bc3185c7745
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2022-01-11 15:40:31 +0000

    :rotating_light: fix lint

--- server.ts
+++ server.ts
@@ -3,7 +3,6 @@
  * Do appinsights first as it does some magic instrumentation work, i.e. it affects other 'require's
  * In particular, applicationinsights automatically collects bunyan logs
  */
-import fs from 'fs'
 import { initialiseAppInsights, buildAppInsightsClient } from './server/utils/azureAppInsights'
 
 initialiseAppInsights()

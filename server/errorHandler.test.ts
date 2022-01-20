1e1a3b8720f7d1def8ce25ec4e67f5b1ba0af56d
Katie Cawthorne <51707463+katie-cawthorne@users.noreply.github.com>
2022-01-20 15:55:49 +0000

    Wfp 820 update error handling (#33)
    * :sparkles: WFP-820: update error page and error handling
    
    * :sparkles: WFP-820: add integration test

--- server/errorHandler.test.ts
+++ server/errorHandler.test.ts
@@ -19,8 +19,8 @@ describe('GET 404', () => {
       .expect(404)
       .expect('Content-Type', /html/)
       .expect(res => {
+        expect(res.text).toContain('Page not found')
         expect(res.text).toContain('NotFoundError: Not found')
-        expect(res.text).not.toContain('Something went wrong. The error has been logged. Please try again')
       })
   })
 
@@ -30,7 +30,7 @@ describe('GET 404', () => {
       .expect(404)
       .expect('Content-Type', /html/)
       .expect(res => {
-        expect(res.text).toContain('Something went wrong. The error has been logged. Please try again')
+        expect(res.text).toContain('Page not found')
         expect(res.text).not.toContain('NotFoundError: Not found')
       })
   })

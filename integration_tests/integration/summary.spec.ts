e3398e940a2aed70a4412ce19c86db02f221c189
Katie Cawthorne <51707463+katie-cawthorne@users.noreply.github.com>
2022-01-27 15:43:38 +0000

    Wfp 787 psr badge (#41)
    * :sparkles: wfp-787: update PSR badge when no PSR created
    
    * :sparkles: wfp-787: update assessment badge when no assessment created

--- integration_tests/integration/summary.spec.ts
+++ integration_tests/integration/summary.spec.ts
@@ -107,8 +107,8 @@ context('Summary', () => {
       PNC: 'A/8404713BA',
       'CPS pack': 'Missing',
       'Pre-convictions': 'Missing',
-      'Pre-sentence report': 'Missing',
-      'Last OASys assessment': 'Missing',
+      'Pre-sentence report': 'No report created',
+      'Last OASys assessment': 'No assessment created',
     })
   })
 

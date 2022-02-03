fd01102a82ad5fd5a40c2cb3d897e20c6eb40176
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2022-02-03 17:12:54 +0000

    :sparkles: wfp-878: add team officer details

--- integration_tests/integration/allocate.spec.ts
+++ integration_tests/integration/allocate.spec.ts
@@ -17,4 +17,62 @@ context('Allocate', () => {
     const allocatePage = Page.verifyOnPage(AllocatePage)
     allocatePage.captionText().should('contain', 'Tier: C1').and('contain', 'CRN: J678910')
   })
+
+  it('Section break is visible on page', () => {
+    cy.task('stubGetUnallocatedCase')
+    cy.signIn()
+    cy.visit('/J678910/case-view')
+    cy.get('a[href*="J678910/allocate"]').click()
+    const allocatePage = Page.verifyOnPage(AllocatePage)
+    allocatePage.sectionBreak().should('exist')
+  })
+
+  it('Sub heading is visible on page', () => {
+    cy.task('stubGetUnallocatedCase')
+    cy.signIn()
+    cy.visit('/J678910/case-view')
+    cy.get('a[href*="J678910/allocate"]').click()
+    const allocatePage = Page.verifyOnPage(AllocatePage)
+    allocatePage.subHeading().should('contain', 'Allocate to a probation practitioner in Wrexham')
+  })
+
+  it('Warning is visible on page if currently managed', () => {
+    cy.task('stubGetUnallocatedCase')
+    cy.signIn()
+    cy.visit('/J678910/case-view')
+    cy.get('a[href*="J678910/allocate"]').click()
+    const allocatePage = Page.verifyOnPage(AllocatePage)
+    allocatePage.warningText().should('contain', 'Dylan Adam Armstrong is currently managed by Antonio LoSardo (SPO)')
+    allocatePage.warningIcon().should('exist')
+  })
+
+  it('Warning is not visible on page if no offender manager details', () => {
+    cy.task('stubGetUnallocatedCaseNoOffenderManager')
+    cy.signIn()
+    cy.visit('/J678910/case-view')
+    cy.get('a[href*="J678910/allocate"]').click()
+    const allocatePage = Page.verifyOnPage(AllocatePage)
+    allocatePage.warningText().should('not.exist')
+    allocatePage.warningIcon().should('not.exist')
+  })
+
+  it('Warning is not visible on page if probation status is Previously managed', () => {
+    cy.task('stubGetUnallocatedCasePreviouslyManaged')
+    cy.signIn()
+    cy.visit('/J678910/case-view')
+    cy.get('a[href*="J678910/allocate"]').click()
+    const allocatePage = Page.verifyOnPage(AllocatePage)
+    allocatePage.warningText().should('not.exist')
+    allocatePage.warningIcon().should('not.exist')
+  })
+
+  it('Warning is not visible on page if probation status is New to probation', () => {
+    cy.task('stubGetUnallocatedCaseNewToProbation')
+    cy.signIn()
+    cy.visit('/J678910/case-view')
+    cy.get('a[href*="J678910/allocate"]').click()
+    const allocatePage = Page.verifyOnPage(AllocatePage)
+    allocatePage.warningText().should('not.exist')
+    allocatePage.warningIcon().should('not.exist')
+  })
 })

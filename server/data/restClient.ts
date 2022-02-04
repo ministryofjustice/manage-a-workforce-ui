61a257b712bf79480d8ddc4dcefdae7e1deda74a
carlov20 <carlov20@users.noreply.github.com>
2022-02-04 13:50:53 +0000

    WFP-870 reverted unrequired change

--- server/data/restClient.ts
+++ server/data/restClient.ts
@@ -64,7 +64,7 @@ export default class RestClient {
     } catch (error) {
       const sanitisedError = sanitiseError(error)
       logger.warn({ ...sanitisedError, query }, `Error calling ${this.name}, path: '${path}', verb: 'GET'`)
-      return Promise.reject(sanitisedError)
+      throw sanitisedError
     }
   }
 

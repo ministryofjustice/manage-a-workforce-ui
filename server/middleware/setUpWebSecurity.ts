8281626ee6ad567c7ab2a0f8698b916ba0ed5ea1
carlov20 <carlov20@users.noreply.github.com>
2022-02-15 09:58:43 +0000

    WFP-863 updated csp

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -15,11 +15,16 @@ export default function setUpWebSecurity(): Router {
           // Hash allows inline script pulled in from https://github.com/alphagov/govuk-frontend/blob/master/src/govuk/template.njk
           scriptSrc: [
             "'self'",
-            'code.jquery.com',
+            "'code.jquery.com'",
             "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
             "'sha256-xseXYIyJf+ofw4QIbNxoWnzeuWkO8antz0n3bwjWrMk='",
           ],
-          styleSrc: ["'self'", 'code.jquery.com'],
+          styleSrc: [
+            "'self'",
+            "'code.jquery.com'",
+            "'sha256-AKswrMHyjd0GSwoPT81/UUJaaYc9Y1jTorRYps5GsQc='",
+            "'sha256-xVy/81YzzTdGdMAj/TciAlD2J5UTOVLOsxo8RFclhQ4='",
+          ],
           fontSrc: ["'self'"],
         },
       },

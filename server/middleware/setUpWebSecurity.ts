3eb35203e4b5d877c8dde50a63b5ecd14239b411
carlov20 <carlov20@users.noreply.github.com>
2022-02-15 10:47:48 +0000

    WFP-863 styling fix

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -19,12 +19,7 @@ export default function setUpWebSecurity(): Router {
             "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
             "'sha256-xseXYIyJf+ofw4QIbNxoWnzeuWkO8antz0n3bwjWrMk='",
           ],
-          styleSrc: [
-            "'self'",
-            'code.jquery.com',
-            "'sha256-AKswrMHyjd0GSwoPT81/UUJaaYc9Y1jTorRYps5GsQc='",
-            "'sha256-xVy/81YzzTdGdMAj/TciAlD2J5UTOVLOsxo8RFclhQ4='",
-          ],
+          styleSrc: ["'self'", 'code.jquery.com'],
           fontSrc: ["'self'"],
         },
       },

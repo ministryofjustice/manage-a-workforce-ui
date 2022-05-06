63e2213956c1bff0976e7363cda6625317cd688d
carlov20 <carlov20@users.noreply.github.com>
2022-05-06 13:29:13 +0100

    WFP-1063

--- assets/js/mojFrontendInit.js
+++ assets/js/mojFrontendInit.js
@@ -1,5 +1,5 @@
 window.MOJFrontend.initAll()
-var $naturalSortableTables = scope.querySelectorAll('[data-module="moj-natural-sortable-table"]')
+var $naturalSortableTables = document.querySelectorAll('[data-module="moj-natural-sortable-table"]')
 MOJFrontend.nodeListForEach($naturalSortableTables, function ($table) {
   new MOJFrontend.NaturalSortableTable({
     table: $table,

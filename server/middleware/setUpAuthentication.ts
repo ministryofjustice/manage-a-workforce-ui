299c9ce49538b06d9e9831e8bea53dd7b4e475e9
carlov20 <carlov20@users.noreply.github.com>
2022-05-27 09:58:34 +0100

    major passport upgrade

--- server/middleware/setUpAuthentication.ts
+++ server/middleware/setUpAuthentication.ts
@@ -30,13 +30,13 @@ export default function setUpAuth(): Router {
 
   const authSignOutUrl = `${config.apis.hmppsAuth.externalUrl}/sign-out?client_id=${config.apis.hmppsAuth.apiClientId}&redirect_uri=${config.domain}`
 
-  router.use('/sign-out', (req, res) => {
+  router.use('/sign-out', (req, res, next) => {
     if (req.user) {
-      req.logout()
-      req.session.destroy(() => res.redirect(authSignOutUrl))
-      return
-    }
-    res.redirect(authSignOutUrl)
+      req.logout(err => {
+        if (err) return next(err)
+        return req.session.destroy(() => res.redirect(authSignOutUrl))
+      })
+    } else res.redirect(authSignOutUrl)
   })
 
   router.use((req, res, next) => {

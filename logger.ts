5f1c5a5d621f39c2a2068ea318483ccd191b87da
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-07-08 15:23:50 +0100

    WFP-1094 do not log exceptions separately to AppInsights - they will be serverity level 3 traces

--- logger.ts
+++ logger.ts
@@ -1,23 +1,15 @@
 import bunyan from 'bunyan'
 import bunyanFormat from 'bunyan-format'
-import { buildAppInsightsClient } from './server/utils/azureAppInsights'
 
 const formatOut = bunyanFormat({ outputMode: 'short', color: true })
 
 const logger = bunyan.createLogger({ name: 'Manage A Workforce Ui', stream: formatOut, level: 'debug' })
 
-const appInsightsClient = buildAppInsightsClient()
-
 export default {
   info(message) {
     logger.info({ message })
   },
-  error(error: Error, message: string) {
-    if (appInsightsClient) {
-      appInsightsClient.trackException({ exception: error })
-    }
-    logger.error(error, message)
-  },
+  error: logger.error.bind(logger),
   warn: logger.warn.bind(logger),
   debug: logger.debug.bind(logger),
 }

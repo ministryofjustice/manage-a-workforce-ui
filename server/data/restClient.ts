a6941e54b536a70dbd22cc8f776a2344f1d0c6b2
AndySweilam <AndySweilam@users.noreply.github.com>
2022-07-21 14:34:42 +0100

    WFP-1160 removed retry from get/post

--- server/data/restClient.ts
+++ server/data/restClient.ts
@@ -57,10 +57,6 @@ export default class RestClient {
       const result = await superagent
         .get(`${this.apiUrl()}${path}`)
         .agent(this.agent)
-        .retry(2, (err, res) => {
-          if (err) logger.info(`Retry handler found API error with ${err.code} ${err.message}`)
-          return undefined // retry handler only for logging retries, not to influence retry logic
-        })
         .query(query)
         .auth(this.token, { type: 'bearer' })
         .set(headers)
@@ -88,10 +84,6 @@ export default class RestClient {
         .post(`${this.apiUrl()}${path}`)
         .send(data)
         .agent(this.agent)
-        .retry(2, (err, res) => {
-          if (err) logger.info(`Retry handler found API error with ${err.code} ${err.message}`)
-          return undefined // retry handler only for logging retries, not to influence retry logic
-        })
         .auth(this.token, { type: 'bearer' })
         .set(headers)
         .responseType(responseType)

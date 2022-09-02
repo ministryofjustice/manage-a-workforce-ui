0cdc5f37fec6bce40fba3ea4125c3c8591c46ed5
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-09-02 07:41:35 +0100

    MAS-1328 reduce wiremock duplication

--- integration_tests/mockApis/probation-estate-wiremock.ts
+++ integration_tests/mockApis/probation-estate-wiremock.ts
@@ -1,13 +1,9 @@
-import superagent, { SuperAgentRequest, Response } from 'superagent'
+import wiremock from './wiremock'
 
 const url = 'http://localhost:9093/__admin'
-
-const stubForProbationEstate = (mapping: Record<string, unknown>): SuperAgentRequest =>
-  superagent.post(`${url}/mappings`).send(mapping)
-
-const getProbationEstateRequests = (): SuperAgentRequest => superagent.get(`${url}/requests`)
-
-const resetProbationEstateStubs = (): Promise<Array<Response>> =>
-  Promise.all([superagent.delete(`${url}/mappings`), superagent.delete(`${url}/requests`)])
-
+const {
+  stubFor: stubForProbationEstate,
+  getRequests: getProbationEstateRequests,
+  resetStubs: resetProbationEstateStubs,
+} = wiremock(url)
 export { stubForProbationEstate, getProbationEstateRequests, resetProbationEstateStubs }

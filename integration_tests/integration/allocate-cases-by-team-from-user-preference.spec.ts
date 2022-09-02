a699b0f7ed3f71ef03f302a7ac5299e2a10d1315
carlov20 <carlov20@users.noreply.github.com>
2022-09-02 14:37:19 +0100

    WFP-1328 added edge cases

--- integration_tests/integration/allocate-cases-by-team-from-user-preference.spec.ts
+++ integration_tests/integration/allocate-cases-by-team-from-user-preference.spec.ts
@@ -65,4 +65,130 @@ context('Show allocate cases by team based on user preferences', () => {
         .and('include', '/PDU1')
     })
   })
+
+  context('Edge cases', () => {
+    beforeEach(() => {
+      cy.task('reset')
+      cy.task('stubSignIn')
+      cy.task('stubAuthUser')
+      cy.task('stubGetAllocations')
+      cy.task('stubUserPreferenceTeams', ['TM1', 'TM2'])
+      cy.task('stubGetTeamsByCodes', {
+        codes: 'TM1,TM2',
+        response: [
+          {
+            code: 'TM1',
+            name: 'Team 1',
+          },
+          {
+            code: 'TM2',
+            name: 'Team 2',
+          },
+        ],
+      })
+      cy.task('stubWorkloadCases', {
+        teamCodes: 'TM1,TM2',
+        response: [
+          {
+            teamCode: 'TM1',
+            totalCases: 3,
+            workload: 77,
+          },
+          {
+            teamCode: 'TM2',
+            totalCases: 4,
+            workload: 88,
+          },
+        ],
+      })
+      cy.task('stubGetUnallocatedCasesByTeams', {
+        teamCodes: 'TM1,TM2',
+        response: [
+          {
+            teamCode: 'TM1',
+            caseCount: 1,
+          },
+          {
+            teamCode: 'TM2',
+            caseCount: 6,
+          },
+        ],
+      })
+      cy.signIn()
+    })
+
+    it('returning no workload for team still displays it', () => {
+      cy.task('stubWorkloadCases', {
+        teamCodes: 'TM1,TM2',
+        response: [
+          {
+            teamCode: 'TM1',
+            totalCases: 3,
+            workload: 77,
+          },
+        ],
+      })
+      cy.visit('/probationDeliveryUnit/PDU1/teams')
+      cy.get('table')
+        .getTable()
+        .should('deep.equal', [
+          {
+            Name: 'Team 1',
+            Workload: '77%',
+            Cases: '3',
+            Action: 'View unallocated cases (1)',
+          },
+          {
+            Name: 'Team 2',
+            Workload: '-%',
+            Cases: '-',
+            Action: 'View unallocated cases (6)',
+          },
+        ])
+    })
+
+    it('returning no allocations for team still displays it', () => {
+      cy.task('stubGetUnallocatedCasesByTeams', {
+        teamCodes: 'TM1,TM2',
+        response: [
+          {
+            teamCode: 'TM1',
+            caseCount: 1,
+          },
+        ],
+      })
+      cy.visit('/probationDeliveryUnit/PDU1/teams')
+      cy.get('table')
+        .getTable()
+        .should('deep.equal', [
+          {
+            Name: 'Team 1',
+            Workload: '77%',
+            Cases: '3',
+            Action: 'View unallocated cases (1)',
+          },
+          {
+            Name: 'Team 2',
+            Workload: '88%',
+            Cases: '4',
+            Action: 'View unallocated cases',
+          },
+        ])
+    })
+
+    it('does not display teams if no probation estate teams', () => {
+      cy.task('stubGetTeamsByCodes', {
+        codes: 'TM1,TM2',
+        response: [],
+      })
+      cy.visit('/probationDeliveryUnit/PDU1/teams')
+      cy.get('table').getTable().should('deep.equal', [])
+    })
+
+    it('does not display teams if no user preferences', () => {
+      cy.task('stubUserPreferenceTeams', [])
+      cy.visit('/probationDeliveryUnit/PDU1/teams')
+      cy.get('table').getTable().should('deep.equal', [])
+    })
+  })
 })
--- server/controllers/allocateCasesController.ts
+++ server/controllers/allocateCasesController.ts
@@ -15,29 +15,32 @@ export default class AllocateCasesController {
   async getDataByTeams(req: Request, res: Response, pduCode: string) {
     const { token, username } = res.locals.user
     const teamCodes = (await this.userPreferenceService.getTeamsUserPreference(token, username)).items
-    const [allocationCasesByTeam, workloadByTeam, probationEstateTeams] = await Promise.all([
-      this.allocationsService.getCaseCountByTeamCodes(token, teamCodes),
-      this.workloadService.getWorkloadByTeams(token, teamCodes),
-      this.probationEstateService.getTeamsByCode(token, teamCodes),
-    ])
-    const caseInformationByTeam = probationEstateTeams
-      .map(team => {
-        const teamWorkload = workloadByTeam.find(w => w.teamCode === team.code) ?? {
-          totalCases: '-',
-          workload: '-',
-        }
-        const teamAllocations = allocationCasesByTeam.find(uc => uc.teamCode === team.code) ?? {
-          caseCount: 0,
-        }
+    let caseInformationByTeam = []
+    if (teamCodes.length) {
+      const [allocationCasesByTeam, workloadByTeam, probationEstateTeams] = await Promise.all([
+        this.allocationsService.getCaseCountByTeamCodes(token, teamCodes),
+        this.workloadService.getWorkloadByTeams(token, teamCodes),
+        this.probationEstateService.getTeamsByCode(token, teamCodes),
+      ])
+      caseInformationByTeam = probationEstateTeams
+        .map(team => {
+          const teamWorkload = workloadByTeam.find(w => w.teamCode === team.code) ?? {
+            totalCases: '-',
+            workload: '-',
+          }
+          const teamAllocations = allocationCasesByTeam.find(uc => uc.teamCode === team.code) ?? {
+            caseCount: 0,
+          }
 
-        return {
-          teamName: team.name,
-          workload: `${teamWorkload.workload}%`,
-          caseCount: teamWorkload.totalCases,
-          unallocatedCaseCount: teamAllocations.caseCount,
-        }
-      })
-      .sort((a, b) => a.teamName.localeCompare(b.teamName))
+          return {
+            teamName: team.name,
+            workload: `${teamWorkload.workload}%`,
+            caseCount: teamWorkload.totalCases,
+            unallocatedCaseCount: teamAllocations.caseCount,
+          }
+        })
+        .sort((a, b) => a.teamName.localeCompare(b.teamName))
+    }
     res.render('pages/allocate-cases-by-team', {
       title: 'Allocate cases by team | Manage a workforce',
       teams: caseInformationByTeam,

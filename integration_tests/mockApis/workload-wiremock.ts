1abc2625b690aa14905900dde31c65e8ab7b0772
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-09-02 07:46:06 +0100

    MAS-1328 reduce wiremock duplication

--- integration_tests/mockApis/workload-wiremock.ts
+++ integration_tests/mockApis/workload-wiremock.ts
@@ -1,13 +1,5 @@
-import superagent, { SuperAgentRequest, Response } from 'superagent'
+import wiremock from './wiremock'
 
 const url = 'http://localhost:9092/__admin'
-
-const stubForWorkload = (mapping: Record<string, unknown>): SuperAgentRequest =>
-  superagent.post(`${url}/mappings`).send(mapping)
-
-const getWorkloadRequests = (): SuperAgentRequest => superagent.get(`${url}/requests`)
-
-const resetWorkloadStubs = (): Promise<Array<Response>> =>
-  Promise.all([superagent.delete(`${url}/mappings`), superagent.delete(`${url}/requests`)])
-
+const { stubFor: stubForWorkload, getRequests: getWorkloadRequests, resetStubs: resetWorkloadStubs } = wiremock(url)
 export { stubForWorkload, getWorkloadRequests, resetWorkloadStubs }

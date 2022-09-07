e278171f0f89f03ade044125b4c73b6cae8fc311
carlov20 <carlov20@users.noreply.github.com>
2022-09-07 10:23:33 +0100

    WFP-1354 fixed test setup

--- integration_tests/integration/empty-unallocated.spec.ts
+++ integration_tests/integration/empty-unallocated.spec.ts
@@ -5,16 +5,16 @@ context('No unallocated cases', () => {
   beforeEach(() => {
     cy.task('reset')
     cy.task('stubSignIn')
+    cy.task('stubGetAllocations', [])
+    cy.signIn()
   })
 
   it('Sub nav link visible on page with no number', () => {
-    cy.signIn()
     const unallocatedPage = Page.verifyOnPage(UnallocatedPage)
     unallocatedPage.subNavLink().should('have.text', 'Unallocated community cases')
   })
 
   it('Must show no case list information available', () => {
-    cy.signIn()
     const unallocatedPage = Page.verifyOnPage(UnallocatedPage)
     unallocatedPage.noCaseParagraph().should('have.text', 'No case list information available.')
   })

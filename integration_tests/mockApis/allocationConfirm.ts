ef17188a59081c5965e06007ab9784fc9eb1f8e6
AndySweilam <AndySweilam@users.noreply.github.com>
2022-09-12 11:35:28 +0100

    WFP-1295 no change to workload for same PoP

--- integration_tests/mockApis/allocationConfirm.ts
+++ integration_tests/mockApis/allocationConfirm.ts
@@ -52,4 +52,29 @@ export default {
       },
     })
   },
+  stubGetPotentialOffenderManagerWorkloadOverCapacitySamePoP: (teamCode = 'TM1'): SuperAgentRequest => {
+    return stubForWorkload({
+      request: {
+        method: 'POST',
+        urlPattern: `/team/${teamCode}/offenderManager/OM1/impact`,
+        bodyPatterns: [
+          {
+            equalToJson: '{"crn":"J678910", "convictionId": 123456789}',
+          },
+        ],
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: {
+          forename: 'John',
+          surname: 'Doe',
+          grade: 'PO',
+          capacity: 50.4,
+          code: 'OM1',
+          potentialCapacity: 50.4,
+        },
+      },
+    })
+  },
 }

064f2b0031a8ec4c65d291e34f816f8856054b1c
carlov20 <carlov20@users.noreply.github.com>
2022-09-14 17:00:09 +0100

    more cleanup

--- integration_tests/mockApis/allocations.ts
+++ integration_tests/mockApis/allocations.ts
@@ -188,10 +188,6 @@ export default {
             completedDate: '2022-03-27T10:54:32.868Z',
             documentId: '22222222-2222-2222-2222-222222222222',
           },
-          assessment: {
-            lastAssessedOn: '2022-01-27T10:54:32.869Z',
-            type: 'LAYER_3',
-          },
           convictionId: 123456789,
           caseType: 'COMMUNITY',
           address: {
@@ -262,10 +258,6 @@ export default {
             completedDate: '2022-03-27T10:54:32.868Z',
             documentId: '22222222-2222-2222-2222-222222222222',
           },
-          assessment: {
-            lastAssessedOn: '2022-01-27T10:54:32.869Z',
-            type: 'LAYER_3',
-          },
           convictionId: 123456789,
           caseType: 'COMMUNITY',
           address: {
@@ -324,10 +316,6 @@ export default {
             completedDate: '2022-01-27T10:54:32.868Z',
             documentId: '00000000-0000-0000-0000-000000000000',
           },
-          assessment: {
-            lastAssessedOn: '2022-01-27T10:54:32.869Z',
-            type: 'LAYER_3',
-          },
           convictionId: 123456789,
           caseType: 'COMMUNITY',
         },
@@ -377,10 +365,6 @@ export default {
             description: 'Fast',
             completedDate: '2022-01-27T10:54:32.868Z',
             documentId: '00000000-0000-0000-0000-000000000000',
-          },
-          assessment: {
-            lastAssessedOn: '2022-01-27T10:54:32.869Z',
-            type: 'LAYER_3',
           },
           convictionId: 123456789,
           caseType: 'COMMUNITY',
@@ -435,10 +419,6 @@ export default {
           cpsPack: {
             completedDate: '2022-02-27T10:54:32.868Z',
             documentId: '11111111-1111-1111-1111-111111111111',
-          },
-          assessment: {
-            lastAssessedOn: '2022-01-27T10:54:32.869Z',
-            type: 'LAYER_3',
           },
         },
         convictionId: 123456789,
--- server/models/Assessment.ts
+++
@@ -1,4 +0,0 @@
-export default interface Assessment {
-  lastAssessedOn: string
-  type: string
-}

8edd7a126757ac25ec4a17ec7c41652e8922a103
carlov20 <carlov20@users.noreply.github.com>
2022-09-21 16:42:07 +0100

    removed unneeded log

--- server/middleware/checkCaseAlreadyAllocated.ts
+++ server/middleware/checkCaseAlreadyAllocated.ts
@@ -1,11 +1,9 @@
 import { Request, Response, NextFunction } from 'express'
 import { HTTPError } from 'superagent'
-import logger from '../../logger'
 import WorkloadService from '../services/workloadService'
 
 export default function checkCaseAlreadyAllocated(workloadService: WorkloadService) {
   return async (error: HTTPError, req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logger.info('In check case already allocated')
     if (error.status === 404) {
       try {
         const { crn, convictionId, teamCode } = req.params

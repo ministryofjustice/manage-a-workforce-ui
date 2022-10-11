9e2fd7d494b21f9a77ecd0c6a51afbce927686b2
carlov20 <carlov20@users.noreply.github.com>
2022-10-11 15:21:11 +0100

    renamed class

--- server/models/ProbationDeliveryUnitDetails.ts
+++ server/models/ProbationDeliveryUnitDetails.ts
@@ -1,7 +1,7 @@
 import EstateRegion from './EstateRegion'
 import EstateTeam from './EstateTeam'
 
-export default interface RegionDetails {
+export default interface ProbationDeliveryUnitDetails {
   code: string
   name: string
   region: EstateRegion

dbe61d14d468a1744df99f250f9a867bb16f26a5
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-10-13 13:00:51 +0100

    WFP-1416 remove unused imports

--- server/models/CaseForChoosePractitioner.ts
+++ server/models/CaseForChoosePractitioner.ts
@@ -1,9 +1,4 @@
 import OffenderManager from './OffenderManager'
-import Offence from './Offence'
-import Requirement from './Requirement'
-import Document from './Document'
-import Assessment from './Assessment'
-import Address from './Address'
 
 export default interface CaseForChoosePractitioner {
   name: string

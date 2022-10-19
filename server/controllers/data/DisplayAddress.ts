f09a21f876ecb658aba8a4db2681a701ceb2e892
carlov20 <carlov20@users.noreply.github.com>
2022-10-19 11:45:13 +0100

    WFP-1362 small refactor for displaying address

--- server/controllers/data/DisplayAddress.ts
+++ server/controllers/data/DisplayAddress.ts
@@ -5,16 +5,15 @@ export default class DisplayAddress {
 
   constructor(address: Address) {
     this.rows = []
-    if (address) {
-      const line1 = `${address.addressNumber ?? ''} ${address.buildingName ?? ''} ${address.streetName ?? ''}`
-        .trim()
-        .replace('  ', ' ')
-      this.rows.push(line1)
-      this.rows.push(address.town)
-      this.rows.push(address.county)
-      this.rows.push(address.postcode)
-      this.rows = this.rows.flatMap(row => (row ? [row] : []))
-    } else {
+    const line1 = `${address?.addressNumber ?? ''} ${address?.buildingName ?? ''} ${address?.streetName ?? ''}`
+      .trim()
+      .replace('  ', ' ')
+    this.rows.push(line1)
+    this.rows.push(address?.town)
+    this.rows.push(address?.county)
+    this.rows.push(address?.postcode)
+    this.rows = this.rows.flatMap(row => (row ? [row] : []))
+    if (this.rows.length === 0) {
       this.rows.push('No address found')
     }
   }

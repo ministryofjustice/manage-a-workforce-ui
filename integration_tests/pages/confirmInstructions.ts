507065bf1d99e59de090e8ddac03d7c62f8196c1
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-11-04 07:40:39 +0000

    WFP-1433: more of rename page files to camelCase

---
+++ integration_tests/pages/confirmInstructions.ts
@@ -0,0 +1,28 @@
+import Page, { PageElement } from './page'
+
+export default class InstructionsConfirmPage extends Page {
+  constructor() {
+    super('Dylan Adam Armstrong')
+  }
+
+  continueButton = (convictionId): PageElement => cy.get(`#${convictionId}`)
+
+  label = (): PageElement => cy.get('.govuk-label')
+
+  hint = (): PageElement => cy.get('.govuk-hint')
+
+  copyText = (): PageElement => cy.get('#copyText')
+
+  addRecipientHeader = (): PageElement => cy.get('.moj-add-another__title')
+
+  addAnotherPersonButton = (): PageElement => cy.get('.moj-add-another__add-button')
+
+  inputTexts = (): PageElement => cy.get('input.govuk-input')
+
+  cancelLink = (crn, convictionId, teamCode): PageElement =>
+    cy.get(`a[href*="/team/${teamCode}/${crn}/convictions/${convictionId}/choose-practitioner"]`).eq(1)
+
+  checkboxText = (): PageElement => cy.get('.govuk-checkboxes__label')
+
+  checkbox = (): PageElement => cy.get('#emailCopy')
+}

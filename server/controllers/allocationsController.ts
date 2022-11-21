a499a74f2c2f22ab6fed46a35b9bbd8f0e17210e
carlov20 <carlov20@users.noreply.github.com>
2022-11-21 16:16:03 +0000

    lint fix

--- server/controllers/allocationsController.ts
+++ server/controllers/allocationsController.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response } from 'express'
 import type { ConfirmInstructionForm } from 'forms'
 import AllocationsService from '../services/allocationsService'
 import Allocation from '../models/Allocation'

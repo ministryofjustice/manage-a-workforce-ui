6eacfbba1b9bc92131c4f8796c79b42353433962
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-11-22 19:12:56 +0000

    FIX: default of indexOf is -1 so no need to set it

--- server/controllers/data/TierOrder.ts
+++ server/controllers/data/TierOrder.ts
@@ -1,3 +1,3 @@
 const tierOrder = ['D0', 'D1', 'D2', 'D3', 'C0', 'C1', 'C2', 'C3', 'B0', 'B1', 'B2', 'B3', 'A0', 'A1', 'A2', 'A3']
 
-export default (tier: string) => tierOrder.indexOf(tier) ?? 0
+export default (tier: string) => tierOrder.indexOf(tier)

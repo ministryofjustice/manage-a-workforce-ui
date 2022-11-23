de605d518e404a72734ec54ea8064db027fefafd
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-11-23 08:34:22 +0000

    FIX: rename tests to match application code

--- integration_tests/pages/allocationComplete.ts
+++ integration_tests/pages/allocationComplete.ts
@@ -1,6 +1,6 @@
 import Page, { PageElement } from './page'
 
-export default class AllocationConfirmPage extends Page {
+export default class AllocationCompletePage extends Page {
   constructor() {
     super('Allocation complete')
   }

d2e19df41ba8cbf3dedc132a2249bd884170def4
carlov20 <carlov20@users.noreply.github.com>
2022-12-02 17:03:55 +0000

    WFP-1647 an option

--- assets/js/instructions.js
+++ assets/js/instructions.js
@@ -1,11 +1,19 @@
 window.onload = function () {
   const textArea = document.getElementById('instructions')
   const convictionId = textArea.getAttribute('data-conviction-id')
-  const instructionsItem = `instructions-save-${convictionId}`
+  const crn = textArea.getAttribute('data-crn')
+  const convictionNumber = textArea.getAttribute('data-conviction-number')
+  const oldInstructionsItem = `instructions-save-${convictionId}`
+  const newInstructionsItem = `instructions-save-${crn}-${convictionNumber}`
   window.onbeforeunload = function () {
-    localStorage.setItem(instructionsItem, textArea.value)
+    localStorage.setItem(newInstructionsItem, textArea.value)
   }
-  if (localStorage[instructionsItem]) {
-    textArea.value = localStorage.getItem(instructionsItem)
+  if (localStorage[oldInstructionsItem]) {
+    const currentValue = localStorage.getItem(oldInstructionsItem)
+    textArea.value = currentValue
+    localStorage.setItem(newInstructionsItem, currentValue)
+  }
+  if (localStorage[newInstructionsItem]) {
+    textArea.value = localStorage.getItem(newInstructionsItem)
   }
 }

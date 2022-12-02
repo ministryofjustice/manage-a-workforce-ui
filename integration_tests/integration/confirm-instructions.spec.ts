f6356b0bc3b57607053764524b7dfc0043e25724
AndySweilam <AndySweilam@users.noreply.github.com>
2022-12-02 15:14:14 +0000

    WFP-1455: use convictionNumber instead of convictionId for confirm-instructions.

--- integration_tests/integration/confirm-instructions.spec.ts
+++ integration_tests/integration/confirm-instructions.spec.ts
@@ -8,27 +8,27 @@ context('Instructions Confirmation', () => {
 
   it('Offender details visible on page', () => {
     cy.task('stubGetStaffByCode')
-    cy.task('stubGetCurrentlyManagedCaseOverview')
+    cy.task('stubGetCurrentlyManagedCaseOverview', '1')
     cy.signIn()
-    cy.visit('/team/TM1/J678910/convictions/123456789/allocate/OM1/instructions')
+    cy.visit('/team/TM1/J678910/convictions/1/allocate/OM1/instructions')
     const instructionsPage = Page.verifyOnPage(InstructionsConfirmPage)
     instructionsPage.captionText().should('contain', 'Tier: C1').and('contain', 'CRN: J678910')
   })
 
   it('Section break is visible on page', () => {
     cy.task('stubGetStaffByCode')
-    cy.task('stubGetCurrentlyManagedCaseOverview')
+    cy.task('stubGetCurrentlyManagedCaseOverview', '1')
     cy.signIn()
-    cy.visit('/team/TM1/J678910/convictions/123456789/allocate/OM1/instructions')
+    cy.visit('/team/TM1/J678910/convictions/1/allocate/OM1/instructions')
     const instructionsPage = Page.verifyOnPage(InstructionsConfirmPage)
     instructionsPage.sectionBreak().should('exist')
   })
 
   it('Breadcrumbs visible on page', () => {
     cy.task('stubGetStaffByCode')
-    cy.task('stubGetCurrentlyManagedCaseOverview')
+    cy.task('stubGetCurrentlyManagedCaseOverview', '1')
     cy.signIn()
-    cy.visit('/team/TM1/J678910/convictions/123456789/allocate/OM1/instructions')
+    cy.visit('/team/TM1/J678910/convictions/1/allocate/OM1/instructions')
     const instructionsPage = Page.verifyOnPage(InstructionsConfirmPage)
     instructionsPage
       .breadCrumbs()
@@ -40,37 +40,37 @@ context('Instructions Confirmation', () => {
 
   it('Allocate Case button visible on page', () => {
     cy.task('stubGetStaffByCode')
-    cy.task('stubGetCurrentlyManagedCaseOverview')
+    cy.task('stubGetCurrentlyManagedCaseOverview', '1')
     cy.signIn()
-    cy.visit('/team/TM1/J678910/convictions/123456789/allocate/OM1/instructions')
+    cy.visit('/team/TM1/J678910/convictions/1/allocate/OM1/instructions')
     const instructionsPage = Page.verifyOnPage(InstructionsConfirmPage)
-    instructionsPage.continueButton('123456789').should('exist').and('have.text', '\n  Allocate case\n')
+    instructionsPage.continueButton('1').should('exist').and('have.text', '\n  Allocate case\n')
   })
 
   it('Continue button goes to next page', () => {
     cy.task('stubGetStaffByCode')
-    cy.task('stubGetCurrentlyManagedCaseOverview')
+    cy.task('stubGetCurrentlyManagedCaseOverview', '1')
     cy.signIn()
-    cy.visit('/team/TM1/J678910/convictions/123456789/allocate/OM1/instructions')
+    cy.visit('/team/TM1/J678910/convictions/1/allocate/OM1/instructions')
     const instructionsPage = Page.verifyOnPage(InstructionsConfirmPage)
-    instructionsPage.continueButton('123456789').click()
-    cy.url().should('include', '/team/TM1/J678910/convictions/123456789/allocate/OM1/confirm-allocation')
+    instructionsPage.continueButton('1').click()
+    cy.url().should('include', '/team/TM1/J678910/convictions/1/allocate/OM1/confirm-allocation')
   })
 
   it('Cancel link visible on page', () => {
     cy.task('stubGetStaffByCode')
-    cy.task('stubGetCurrentlyManagedCaseOverview')
+    cy.task('stubGetCurrentlyManagedCaseOverview', '1')
     cy.signIn()
-    cy.visit('/team/TM1/J678910/convictions/123456789/allocate/OM1/instructions')
+    cy.visit('/team/TM1/J678910/convictions/1/allocate/OM1/instructions')
     const instructionsPage = Page.verifyOnPage(InstructionsConfirmPage)
-    instructionsPage.cancelLink('J678910', '123456789', 'TM1').should('exist').and('have.text', 'Cancel')
+    instructionsPage.cancelLink('J678910', '1', 'TM1').should('exist').and('have.text', 'Cancel')
   })
 
   it('Instructions textArea should be visible on page', () => {
     cy.task('stubGetStaffByCode')
-    cy.task('stubGetCurrentlyManagedCaseOverview')
+    cy.task('stubGetCurrentlyManagedCaseOverview', '1')
     cy.signIn()
-    cy.visit('/team/TM1/J678910/convictions/123456789/allocate/OM1/instructions')
+    cy.visit('/team/TM1/J678910/convictions/1/allocate/OM1/instructions')
     const instructionsPage = Page.verifyOnPage(InstructionsConfirmPage)
     instructionsPage.instructionsTextArea().should('exist').and('have.attr', 'rows', '20')
     instructionsPage.subHeading().should('contain', 'Review allocation instructions')
@@ -83,9 +83,9 @@ context('Instructions Confirmation', () => {
 
   it('another copy text should be visible on page', () => {
     cy.task('stubGetStaffByCode')
-    cy.task('stubGetCurrentlyManagedCaseOverview')
+    cy.task('stubGetCurrentlyManagedCaseOverview', '1')
     cy.signIn()
-    cy.visit('/team/TM1/J678910/convictions/123456789/allocate/OM1/instructions')
+    cy.visit('/team/TM1/J678910/convictions/1/allocate/OM1/instructions')
     const instructionsPage = Page.verifyOnPage(InstructionsConfirmPage)
     instructionsPage
       .copyText()
@@ -94,18 +94,18 @@ context('Instructions Confirmation', () => {
 
   it('add another recipient should be visible on page', () => {
     cy.task('stubGetStaffByCode')
-    cy.task('stubGetCurrentlyManagedCaseOverview')
+    cy.task('stubGetCurrentlyManagedCaseOverview', '1')
     cy.signIn()
-    cy.visit('/team/TM1/J678910/convictions/123456789/allocate/OM1/instructions')
+    cy.visit('/team/TM1/J678910/convictions/1/allocate/OM1/instructions')
     const instructionsPage = Page.verifyOnPage(InstructionsConfirmPage)
     instructionsPage.addRecipientHeader().should('contain', 'Add another recipient')
   })
 
   it('adding another person adds more email address inputs', () => {
     cy.task('stubGetStaffByCode')
-    cy.task('stubGetCurrentlyManagedCaseOverview')
+    cy.task('stubGetCurrentlyManagedCaseOverview', '1')
     cy.signIn()
-    cy.visit('/team/TM1/J678910/convictions/123456789/allocate/OM1/instructions')
+    cy.visit('/team/TM1/J678910/convictions/1/allocate/OM1/instructions')
     const instructionsPage = Page.verifyOnPage(InstructionsConfirmPage)
     instructionsPage.inputTexts().should('have.length', 1)
     instructionsPage.addAnotherPersonButton().click()

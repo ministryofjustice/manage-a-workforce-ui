770f6df4db2e68dbcaeb0a279dc18a5ef2531e3c
carlov20 <carlov20@users.noreply.github.com>
2022-12-06 08:37:00 +0000

    WFP-1652 use person name from case overview (#570)

--- server/models/PersonManager.ts
+++ server/models/PersonManager.ts
@@ -1,12 +1,4 @@
 export default interface PersonManager {
-  staffId: number
-  staffCode: string
-  teamCode: string
-  providerCode: string
-  createdBy: string
-  createdDate: string
-  crn: string
-  personName: string
   staffGrade: string
   staffEmail: string
   staffForename: string

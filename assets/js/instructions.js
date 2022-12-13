13eed9c3db0991e1362ed6319f4f1d6d8f7a0421
carlov20 <carlov20@users.noreply.github.com>
2022-12-13 09:02:09 +0000

    WFP-1650 remove conviction ID from instructions

--- assets/js/instructions.js
+++ assets/js/instructions.js
@@ -1,19 +1,12 @@
 window.onload = function () {
   const textArea = document.getElementById('instructions')
-  const convictionId = textArea.getAttribute('data-conviction-id')
   const crn = textArea.getAttribute('data-crn')
   const convictionNumber = textArea.getAttribute('data-conviction-number')
-  const oldInstructionsItem = `instructions-save-${convictionId}`
   const newInstructionsItem = `instructions-save-${crn}-${convictionNumber}`
   window.onbeforeunload = function () {
     localStorage.setItem(newInstructionsItem, textArea.value)
   }
   if (localStorage[newInstructionsItem]) {
     textArea.value = localStorage.getItem(newInstructionsItem)
-  } else if (localStorage[oldInstructionsItem]) {
-    const currentValue = localStorage.getItem(oldInstructionsItem)
-    textArea.value = currentValue
-    localStorage.setItem(newInstructionsItem, currentValue)
-    localStorage.removeItem(oldInstructionsItem)
   }
 }

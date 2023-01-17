6d06d242dbc0a9deb842fa139e7c95810042f15c
carlov20 <carlov20@users.noreply.github.com>
2023-01-17 10:11:31 +0000

    WFP-1711 error handling for no values form submit (#603)

--- server/controllers/findUnallocatedCasesController.ts
+++ server/controllers/findUnallocatedCasesController.ts
@@ -1,15 +1,36 @@
-import { Response } from 'express'
+import { Request, Response } from 'express'
+import type { FindUnallocatedCasesForm } from 'forms'
 import ProbationEstateService from '../services/probationEstateService'
+import validate from '../validation/validation'
+import trimForm from '../utils/trim'
 
 export default class FindUnallocatedCasesController {
   constructor(private readonly probationEstateService: ProbationEstateService) {}
 
-  async findUnallocatedCases(_, res: Response, pduCode: string): Promise<void> {
+  async findUnallocatedCases(req: Request, res: Response, pduCode: string): Promise<void> {
     const { token } = res.locals.user
     const pduDetails = await this.probationEstateService.getProbationDeliveryUnitDetails(token, pduCode)
     res.render('pages/find-unallocated-cases', {
       pduDetails,
       title: 'Unallocated cases | Manage a workforce',
+      errors: req.flash('errors') || [],
     })
+  }
+
+  async submitFindUnallocatedCases(req: Request, res: Response, pduCode: string, form): Promise<void> {
+    const findUnallocatedCasesForm = trimForm<FindUnallocatedCasesForm>(form)
+    const errors = validate(
+      findUnallocatedCasesForm,
+      { pdu: 'required', ldu: 'required', team: 'required' },
+      {
+        'required.pdu': 'Select a Probation Delivery Unit',
+        'required.ldu': 'Select a Local Delivery Unit',
+        'required.team': 'Select a team',
+      }
+    )
+
+    req.flash('errors', errors)
+    // eslint-disable-next-line security-node/detect-dangerous-redirects
+    res.redirect(`/probationDeliveryUnit/${pduCode}/find-unallocated`)
   }
 }

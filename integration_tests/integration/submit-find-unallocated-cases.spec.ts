79209066ae052482b8f34b04d933ba76f90a637c
carlov20 <carlov20@users.noreply.github.com>
2023-01-17 13:59:49 +0000

    WFP-1711 (#605)
    * WFP-1711 added getting all estate
    
    * removed unused import
    
    * added stub
    
    * WFP-1711 added selection in ui
    
    * merge
    
    * error path
    
    * another test

--- integration_tests/integration/submit-find-unallocated-cases.spec.ts
+++ integration_tests/integration/submit-find-unallocated-cases.spec.ts
@@ -21,4 +21,88 @@ context('Submit find Unallocated cases', () => {
         'There is a problem Select a Probation Delivery Unit Select a Local Delivery Unit Select a team'
       )
   })
+
+  it('selecting PDU populates LDU with correct options', () => {
+    findUnallocatedCasesPage.select('pdu').select('PDU1')
+    findUnallocatedCasesPage
+      .select('ldu')
+      .getOptions()
+      .should('deep.equal', [
+        {
+          optionValue: '',
+          optionContent: 'Select LDU',
+        },
+        {
+          optionValue: 'LDU1',
+          optionContent: 'First Local Delivery Unit',
+        },
+        {
+          optionValue: 'LDU2',
+          optionContent: 'Second Local Delivery Unit',
+        },
+      ])
+  })
+
+  it('selecting LDU populates Team with correct options', () => {
+    findUnallocatedCasesPage.select('pdu').select('PDU1')
+    findUnallocatedCasesPage.select('ldu').select('LDU1')
+    findUnallocatedCasesPage
+      .select('team')
+      .getOptions()
+      .should('deep.equal', [
+        {
+          optionValue: '',
+          optionContent: 'Select team',
+        },
+        {
+          optionValue: 'TM1',
+          optionContent: 'First Team',
+        },
+        {
+          optionValue: 'TM2',
+          optionContent: 'Second Team',
+        },
+      ])
+  })
+
+  it('unselecting PDU clears LDU and team', () => {
+    findUnallocatedCasesPage.select('pdu').select('PDU1')
+    findUnallocatedCasesPage.select('ldu').select('LDU1')
+    findUnallocatedCasesPage.select('team').select('TM1')
+    findUnallocatedCasesPage.select('pdu').select(0)
+    findUnallocatedCasesPage
+      .select('ldu')
+      .getOptions()
+      .should('deep.equal', [
+        {
+          optionValue: '',
+          optionContent: 'Select LDU',
+        },
+      ])
+    findUnallocatedCasesPage
+      .select('team')
+      .getOptions()
+      .should('deep.equal', [
+        {
+          optionValue: '',
+          optionContent: 'Select team',
+        },
+      ])
+  })
+
+  it('unselecting LDU clears team', () => {
+    findUnallocatedCasesPage.select('pdu').select('PDU1')
+    findUnallocatedCasesPage.select('ldu').select('LDU1')
+    findUnallocatedCasesPage.select('team').select('TM1')
+    findUnallocatedCasesPage.select('ldu').select(0)
+    findUnallocatedCasesPage
+      .select('team')
+      .getOptions()
+      .should('deep.equal', [
+        {
+          optionValue: '',
+          optionContent: 'Select team',
+        },
+      ])
+  })
 })

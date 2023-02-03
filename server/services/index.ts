885eadbca9db24afdf9f2f1498375f5fc6470e57
carlov20 <carlov20@users.noreply.github.com>
2023-02-03 13:57:39 +0000

    WFP-1755 added all setup

--- server/services/index.ts
+++ server/services/index.ts
@@ -5,6 +5,7 @@ import WorkloadService from './workloadService'
 import ProbationEstateService from './probationEstateService'
 import UserPreferenceService from './userPreferenceService'
 import config from '../config'
+import StaffLookupService from './staffLookupService'
 
 export const services = () => {
   const { hmppsAuthClient } = dataAccess()
@@ -14,12 +15,14 @@ export const services = () => {
   const workloadService = new WorkloadService(config.apis.workloadService)
   const probationEstateService = new ProbationEstateService(config.apis.probationEstateService)
   const userPreferenceService = new UserPreferenceService(config.apis.userPreferenceService)
+  const staffLookupService = new StaffLookupService(config.apis.staffLookupService)
   return {
     userService,
     allocationsService,
     workloadService,
     probationEstateService,
     userPreferenceService,
+    staffLookupService,
   }
 }
 

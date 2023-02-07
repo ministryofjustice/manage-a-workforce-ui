2fcabd8a2bddb73a234ea1b7bf7f64ef7a339070
carlov20 <carlov20@users.noreply.github.com>
2023-02-07 10:43:11 +0000

    Revert "moved app insights to config file (#658)" (#659)
    This reverts commit fbd12bfe48fd21309130e89780f8ffe4e7106c0f.

--- server/data/index.ts
+++ server/data/index.ts
@@ -3,9 +3,10 @@
  * Do appinsights first as it does some magic instrumentation work, i.e. it affects other 'require's
  * In particular, applicationinsights automatically collects bunyan logs
  */
-import initialiseAppInsights from '../utils/azureAppInsights'
+import { initialiseAppInsights, buildAppInsightsClient } from '../utils/azureAppInsights'
 
 initialiseAppInsights()
+buildAppInsightsClient()
 
 import HmppsAuthClient from './hmppsAuthClient'
 
--- server/utils/azureAppInsights.ts
+++ server/utils/azureAppInsights.ts
@@ -1,10 +1,32 @@
-import { setup, DistributedTracingModes } from 'applicationinsights'
+import { setup, defaultClient, TelemetryClient, DistributedTracingModes } from 'applicationinsights'
+import applicationVersion from '../applicationVersion'
 
-export default function initialiseAppInsights(): void {
+function defaultName(): string {
+  const {
+    packageData: { name },
+  } = applicationVersion
+  return name
+}
+
+function version(): string {
+  const { buildNumber } = applicationVersion
+  return buildNumber
+}
+
+export function initialiseAppInsights(): void {
   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
     // eslint-disable-next-line no-console
     console.log('Enabling azure application insights')
 
     setup().setDistributedTracingMode(DistributedTracingModes.AI_AND_W3C).start()
   }
+}
+
+export function buildAppInsightsClient(name = defaultName()): TelemetryClient {
+  if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+    defaultClient.context.tags['ai.cloud.role'] = name
+    defaultClient.context.tags['ai.application.ver'] = version()
+    return defaultClient
+  }
+  return null
 }

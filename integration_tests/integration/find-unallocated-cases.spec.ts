8c6fdd1339c2ddda91f3eca5d47abb7e7c9a1eab
AndySweilam <AndySweilam@users.noreply.github.com>
2023-03-30 12:00:25 +0100

    WFP-1916: Add home breadcrumbs to find-unallocated-cases

--- integration_tests/integration/find-unallocated-cases.spec.ts
+++ integration_tests/integration/find-unallocated-cases.spec.ts
@@ -50,6 +50,10 @@ context('Find Unallocated cases', () => {
     findUnallocatedCasesPage.subNav().should('contain', 'Unallocated community cases')
   })
 
+  it('Breadcrumbs visible on page', () => {
+    findUnallocatedCasesPage.breadCrumbs().should('contain', 'Home')
+  })
+
   it('Shows link to Edit my teams list', () => {
     findUnallocatedCasesPage.manageMyTeamsLink().should('equal', '/pdu/PDU1/select-teams')
   })

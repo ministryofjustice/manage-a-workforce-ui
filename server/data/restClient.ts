0bde3d73718203bc2176876e97a11d585b7921b1
Gary Reid <gary.reid@digital.justice.gov.uk>
2024-06-18 13:12:09 +0100

    lint error

--- server/data/restClient.ts
+++ server/data/restClient.ts
@@ -40,11 +40,7 @@ export default class RestClient {
 
   axiosClient: AxiosInstance
 
-  constructor(
-    private readonly name: string,
-    private readonly config: ApiConfig,
-    private readonly token: string
-  ) {
+  constructor(private readonly name: string, private readonly config: ApiConfig, private readonly token: string) {
     this.agent = config.url.startsWith('https') ? new HttpsAgent(config.agent) : new Agent(config.agent)
     this.axiosClient = axios.create({
       baseURL: config.url,

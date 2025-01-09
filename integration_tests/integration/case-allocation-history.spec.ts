bb421131104b964bc468f49ea9242552d8bdd46f
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2025-01-09 13:19:31 +0000

    Epic 30 days error with excluded cases (#1042)
    * WFP-2885 ensuring right LAO display in past 30 days allocations report
    
    * changing view to fix 30 day lao display
    
    * 30 days fixing to show details of POP for lao case allocated
    
    * Changing formatting for restricted access badge
    
    * removing spare div
    
    * Make name normal weight
    
    * Changing bottom margin on CRN for 30 day report
    
    * changing font weight and margin
    
    * font weight for regular cases names
    
    * missing bracket
    
    * Sorting formatting name text weight
    
    * fix test
    
    * making all cases sortable
    
    * changing margin bottom of CRN on LAO to 10px
    
    * fix test due to sortable table
    
    * Adding test for restricted badge

--- integration_tests/integration/case-allocation-history.spec.ts
+++ integration_tests/integration/case-allocation-history.spec.ts
@@ -9,7 +9,7 @@ context('Case allocation history', () => {
   beforeEach(() => {
     cy.task('stubSetup')
     cy.task('stubForGetLaoRestrictions', { crn: 'N04A123' })
-    cy.task('stubForGetLaoRestrictions', { crn: 'X602047' })
+    cy.task('stubForGetLaoRestrictionsExcluded', { crn: 'X602047' })
     cy.task('stubForGetLaoRestrictions', { crn: 'X602070' })
     cy.task('stubCaseAllocationHistory')
     cy.signIn()
@@ -120,14 +120,14 @@ context('Case allocation history', () => {
         })
         expect(trimmedActualTableData).to.deep.equal([
           {
-            'Name / CRN': 'Terrance Yundt                    X602070',
+            'Name / CRN': 'Terrance Yundt                            X602070',
             Tier: 'D0',
             'Date allocated': '3 March 2023',
             'Probation Practitioner': 'Andy Pandy',
             'Allocated by': 'N04A124',
           },
           {
-            'Name / CRN': 'Stacy Koepp                    X602047',
+            'Name / CRN': 'Stacy Koepp                            X602047                    Restricted access',
             Tier: 'C1',
             'Date allocated': '2 February 2023',
             'Probation Practitioner': 'Steve Leave',
@@ -146,6 +146,10 @@ context('Case allocation history', () => {
     cy.get('#allCases').should('exist')
     caseAllocationHistoryPage.noCasesBody().should('exist')
     caseAllocationHistoryPage.noCasesBody().should('contain.text', 'There are no cases allocated in the past 30 days.')
+  })
+
+  it('shows restricted badge and warning text if case has restricted access', () => {
+    caseAllocationHistoryPage.restrictedBadge().should('exist')
   })
 
   it('This tab is highlighted', () => {

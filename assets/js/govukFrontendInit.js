56ed3a7c7652abe0f9f8a5c09bb1415012d1e125
Gary Reid <gary.reid@digital.justice.gov.uk>
2025-02-11 11:47:21 +0000

    WFP-2929 Officer view breadcrumb

--- assets/js/govukFrontendInit.js
+++ assets/js/govukFrontendInit.js
@@ -1,2 +1,6 @@
 import { initAll } from '/assets/govuk/govuk-frontend.min.js'
 initAll()
+
+document.querySelectorAll('a[data-back]').forEach(elem => {
+  elem.addEventListener('click', () => window.history.back())
+})

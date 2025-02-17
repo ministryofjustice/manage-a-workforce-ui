1b50723e906d306e64597d37e0e29333b66601af
Gary Reid <gary.reid@digital.justice.gov.uk>
2025-02-17 16:00:49 +0000

    WFP-2941 prevent multiple clicks

--- assets/js/govukFrontendInit.js
+++ assets/js/govukFrontendInit.js
@@ -6,10 +6,11 @@ document.querySelectorAll('a[data-back]').forEach(elem => {
 })
 
 document.querySelectorAll('button[data-disable]').forEach(elem => {
-  elem.addEventListener('click', () =>
+  elem.addEventListener('click', () => {
     document.querySelectorAll('button[data-disable]').forEach(item => {
       item.disabled = true
     })
-  )
-  elem.form.submit()
+
+    elem.form.submit()
+  })
 })

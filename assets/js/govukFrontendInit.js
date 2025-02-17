5c565e468b18dcf7d34ef2c8289a9695dfb24306
Gary Reid <gary.reid@digital.justice.gov.uk>
2025-02-17 15:33:46 +0000

    WFP-2941 multiplke clicks part 1

--- assets/js/govukFrontendInit.js
+++ assets/js/govukFrontendInit.js
@@ -4,3 +4,12 @@ initAll()
 document.querySelectorAll('a[data-back]').forEach(elem => {
   elem.addEventListener('click', () => window.history.back())
 })
+
+document.querySelectorAll('button[data-disable]').forEach(elem => {
+  elem.addEventListener('click', () =>
+    document.querySelectorAll('button[data-disable]').forEach(item => {
+      item.disabled = true
+    })
+  )
+  elem.form.submit()
+})

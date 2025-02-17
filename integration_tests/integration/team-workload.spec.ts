74b7e14ec95ae4cac4f0965526ad9bc110dc60ca
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-02-17 18:03:07 +0000

    fixing tests

--- integration_tests/integration/team-workload.spec.ts
+++ integration_tests/integration/team-workload.spec.ts
@@ -8,39 +8,105 @@ context('Team Workload', () => {
     cy.task('stubSetup')
     cy.signIn()
     cy.task('stubGetTeamDetails', { code: 'N03F01', name: 'Team Name 1' })
-    cy.task('stubForTeamWorkload')
-    cy.visit('/pdu/PDU1/N03F01/team-workload')
-
-    teamWorkloadPage = Page.verifyOnPage(TeamWorkloadPage)
   })
 
   it('Team details visible on page', () => {
+    cy.task('stubForTeamWorkload')
+    cy.task('stubWorkloadCases', {
+      teamCodes: 'N03F01',
+      response: [
+        {
+          teamCode: 'N03F01',
+          totalCases: 3,
+          workload: 77,
+        },
+      ],
+    })
+    cy.visit('/pdu/PDU1/N03F01/team-workload')
+
+    teamWorkloadPage = Page.verifyOnPage(TeamWorkloadPage)
     teamWorkloadPage.captionText().should('contain', 'A Probation Delivery Unit')
     teamWorkloadPage.heading().should('contain', 'Team Name 1')
     teamWorkloadPage.mediumHeading().should('contain', 'Workload')
   })
 
-  it.skip('calculates the correct total cases and average workload', () => {
+  it('calculates the correct total cases and average workload', () => {
+    cy.task('stubForTeamWorkload')
+    cy.task('stubWorkloadCases', {
+      teamCodes: 'N03F01',
+      response: [
+        {
+          teamCode: 'N03F01',
+          totalCases: 210,
+          workload: 75,
+        },
+      ],
+    })
+    cy.visit('/pdu/PDU1/N03F01/team-workload')
+
+    teamWorkloadPage = Page.verifyOnPage(TeamWorkloadPage)
     teamWorkloadPage.totalCases().should('contain.text', '210')
     teamWorkloadPage.averageWorkload().should('contain.text', '75%')
     teamWorkloadPage.averageWorkload().should('have.class', 'under-capacity')
   })
 
   it('shows the right number of rows in the table', () => {
+    cy.task('stubWorkloadCases', {
+      teamCodes: 'N03F01',
+      response: [
+        {
+          teamCode: 'N03F01',
+          totalCases: 3,
+          workload: 77,
+        },
+      ],
+    })
+    cy.task('stubForTeamWorkload')
+    cy.visit('/pdu/PDU1/N03F01/team-workload')
+
+    teamWorkloadPage = Page.verifyOnPage(TeamWorkloadPage)
     teamWorkloadPage.teamTableRows().should('have.length', 14)
     teamWorkloadPage.teamTableRows().first().should('contain.text', 'Coco Pint')
     teamWorkloadPage.teamTableRows().first().should('contain.text', 'PO')
     teamWorkloadPage.teamTableRows().first().should('contain.text', '10%')
   })
 
-  it.skip('adds the over-capacity class to average workload', () => {
+  it('adds the over-capacity class to average workload', () => {
+    cy.task('stubWorkloadCases', {
+      teamCodes: 'N03F01',
+      response: [
+        {
+          teamCode: 'N03F01',
+          totalCases: 3,
+          workload: 110,
+        },
+      ],
+    })
+    cy.task('stubForTeamWorkload')
+    cy.visit('/pdu/PDU1/N03F01/team-workload')
+
+    teamWorkloadPage = Page.verifyOnPage(TeamWorkloadPage)
     cy.task('stubForTeamWorkloadOverCapacity')
     cy.visit('/pdu/PDU1/N03F01/team-workload')
 
     teamWorkloadPage.averageWorkload().should('have.class', 'over-capacity')
   })
 
-  it.skip('filters out 0% workloads', () => {
+  it('filters out 0% workloads', () => {
+    cy.task('stubWorkloadCases', {
+      teamCodes: 'N03F01',
+      response: [
+        {
+          teamCode: 'N03F01',
+          totalCases: 100,
+          workload: 75,
+        },
+      ],
+    })
+    cy.task('stubForTeamWorkload')
+    cy.visit('/pdu/PDU1/N03F01/team-workload')
+
+    teamWorkloadPage = Page.verifyOnPage(TeamWorkloadPage)
     cy.task('stubForTeamWorkloadZeroCapacities')
     cy.visit('/pdu/PDU1/N03F01/team-workload')
 

fd05a6ffd58c8cd006eab693b2682c9ae5824bcc
Gary Reid <gary.reid@digital.justice.gov.uk>
2025-02-24 15:38:24 +0000

    WFP-2941 disable buttons in validation script, so as not to duplicate

--- assets/js/govukFrontendInit.js
+++ assets/js/govukFrontendInit.js
@@ -4,39 +4,3 @@ initAll()
 document.querySelectorAll('a[data-back]').forEach(elem => {
   elem.addEventListener('click', () => window.history.back())
 })
-
-document.querySelectorAll('button[data-disable]').forEach(elem => {
-  elem.addEventListener('click', () => {
-    document.querySelectorAll('button[data-disable]').forEach(item => {
-      item.disabled = true
-    })
-
-    elem.form.submit()
-  })
-})
-
-document.querySelectorAll('button[data-disable2]').forEach(elem => {
-  elem.addEventListener('click', event => {
-    // Perform validation before submitting the form
-    const form = elem.form
-    const value = document.getElementById('instructions').value
-    const isValid =
-      !/((https?|ftp|smtp):\/\/|www\.)([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])/g.test(value)
-
-    if (!isValid) {
-      event.preventDefault()
-      return
-    }
-    if (!value || value.trim().length == 0) {
-      event.preventDefault()
-      return
-    }
-
-    console.info('valid =' + isValid)
-    document.querySelectorAll('button[data-disable2]').forEach(item => {
-      item.disabled = true
-    })
-
-    elem.form.submit()
-  })
-})

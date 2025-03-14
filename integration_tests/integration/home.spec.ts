0b957b62f3f1422b81d556356e4655a042a24d2c
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-03-14 09:53:33 +0000

    wfp-2981-small tweak and fix test

--- integration_tests/integration/home.spec.ts
+++ integration_tests/integration/home.spec.ts
@@ -32,16 +32,10 @@ context('Unallocated', () => {
     allocateCasesByTeamPage.headerUserName().should('contain.text', 'J. Smith')
   })
 
-  it('Feedback link goes to Microsoft forms survey', () => {
+  it('Feedback link goes to Smart Survey survey', () => {
     cy.signIn()
     const allocateCasesByTeamPage = Page.verifyOnPage(AllocateCasesByTeamPage)
-    allocateCasesByTeamPage
-      .surveyLink()
-      .should('have.attr', 'href')
-      .and(
-        'equal',
-        'https://forms.office.com/Pages/ResponsePage.aspx?id=KEeHxuZx_kGp4S6MNndq2JfL5_1h2G9Gi17tzGJyJ5hUMjI3UDgwVzgwS1JLV0hXNDFOTjc4Q1FCQi4u'
-      )
+    allocateCasesByTeamPage.surveyLink().should('have.attr', 'href').and('contain', 'www.smartsurvey.co.uk/s/AWQG5Z')
   })
 
   it('Footer visible on page', () => {

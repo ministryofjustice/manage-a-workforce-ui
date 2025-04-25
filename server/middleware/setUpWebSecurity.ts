44fd27f095a4000279c89f85e691178bdd41f386
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-04-25 09:51:26 +0100

    tighten csp

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -11,52 +11,62 @@ export default function setUpWebSecurity(): Router {
   })
 
   router.use((req: Request, res: Response, next: NextFunction) => {
-    const cspMiddleware = helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'", 'https://www.google-analytics.com', 'www.google-analytics.com'],
-        scriptSrc: [
-          "'self'",
-          'code.jquery.com',
-          'www.smartsurvey.co.uk',
-          'www.googletagmanager.com',
-          'www.google-analytics.com',
-          'https://www.google-analytics.com',
-          'https://www.googletagmanager.com',
-          'js.monitor.azure.com',
-          "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
-          "'sha256-xseXYIyJf+ofw4QIbNxoWnzeuWkO8antz0n3bwjWrMk='",
-          `'nonce-${res.locals.cspNonce}'`,
-        ],
-        styleSrc: [
-          "'self'",
-          '*.smartsurvey.co.uk',
-          `'nonce-${res.locals.cspNonce}'`,
-          "'unsafe-hashes'",
-          "'sha256-uLscbKA9leLZ+gAcOlaJsUafZAw8nl4mzJuREYz2ZoQ='",
-          "'sha256-qnVkQSG7pWu17hBhIw0kCpfEB3XGvt0mNRa6+uM6OUU='",
-        ],
-        fontSrc: ["'self'", '*.smartsurvey.co.uk'],
-        imgSrc: [
-          "'self'",
-          'https://www.google-analytics.com',
-          'www.google-analytics.com',
-          '*.analytics.google.com',
-          '*.google-analytics.com',
-          '*.googletagmanager.com',
-          '*.smartsurvey.co.uk',
-        ],
-        connectSrc: [
-          "'self'",
-          'www.googletagmanager.com',
-          'www.google-analytics.com',
-          'https://www.google-analytics.com',
-          '*.analytics.google.com',
-          '*.google-analytics.com',
-          'js.monitor.azure.com',
-          'dc.services.visualstudio.com',
-          'smartsurvey.co.uk',
-        ],
+    const cspMiddleware = helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'", 'https://www.google-analytics.com', 'www.google-analytics.com'],
+          scriptSrc: [
+            "'self'",
+            'code.jquery.com',
+            'www.smartsurvey.co.uk',
+            'www.googletagmanager.com',
+            'www.google-analytics.com',
+            'https://www.google-analytics.com',
+            'https://www.googletagmanager.com',
+            'js.monitor.azure.com',
+            "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
+            "'sha256-xseXYIyJf+ofw4QIbNxoWnzeuWkO8antz0n3bwjWrMk='",
+            `'nonce-${res.locals.cspNonce}'`,
+          ],
+          styleSrc: [
+            "'self'",
+            '*.smartsurvey.co.uk',
+            `'nonce-${res.locals.cspNonce}'`,
+            "'unsafe-hashes'",
+            "'sha256-uLscbKA9leLZ+gAcOlaJsUafZAw8nl4mzJuREYz2ZoQ='",
+            "'sha256-qnVkQSG7pWu17hBhIw0kCpfEB3XGvt0mNRa6+uM6OUU='",
+          ],
+          fontSrc: ["'self'", '*.smartsurvey.co.uk'],
+          imgSrc: [
+            "'self'",
+            'https://www.google-analytics.com',
+            'www.google-analytics.com',
+            '*.analytics.google.com',
+            '*.google-analytics.com',
+            '*.googletagmanager.com',
+            '*.smartsurvey.co.uk',
+          ],
+          connectSrc: [
+            "'self'",
+            'www.googletagmanager.com',
+            'www.google-analytics.com',
+            'https://www.google-analytics.com',
+            '*.analytics.google.com',
+            '*.google-analytics.com',
+            'js.monitor.azure.com',
+            'dc.services.visualstudio.com',
+            'smartsurvey.co.uk',
+          ],
+        },
       },
+      referrerPolicy: { policy: 'strict-origin-when-cross-origin' },
+      frameguard: { action: 'deny' },
+      hsts: {
+        maxAge: 31536000,
+        includeSubDomains: true,
+        preload: true,
+      },
+      xssFilter: true,
     })
 
     cspMiddleware(req, res, next)

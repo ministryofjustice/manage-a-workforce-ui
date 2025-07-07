97c882882fb8a2655fce75a57ef31fd013d9d6fa
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-07-07 15:42:03 +0100

    logic to check for kubernetes environment

--- preload-opentelemetry.ts
+++ preload-opentelemetry.ts
@@ -3,21 +3,23 @@ import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentation
 import { AzureMonitorTraceExporter } from '@azure/monitor-opentelemetry-exporter'
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base'
 
-const key = process.env.APPINSIGHTS_INSTRUMENTATIONKEY
-const connStr = `InstrumentationKey=${key};IngestionEndpoint=https://northeurope-0.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;`
+if (process.env.NODE_ENV === 'production') {
+  const key = process.env.APPINSIGHTS_INSTRUMENTATIONKEY
+  const connStr = `InstrumentationKey=${key};IngestionEndpoint=https://northeurope-0.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;`
 
-// Prevent double initialization
-global.otelStarted = global.otelStarted || false
+  // Prevent double initialization
+  global.otelStarted = global.otelStarted || false
 
-if (!global.otelStarted) {
-  if (connStr) {
-    const exporter = new AzureMonitorTraceExporter({ connectionString: connStr })
-    const sdk = new NodeSDK({
-      serviceName: 'manage-a-workforce-ui',
-      spanProcessors: [new BatchSpanProcessor(exporter)],
-      instrumentations: [getNodeAutoInstrumentations()],
-    })
-    sdk.start()
+  if (!global.otelStarted) {
+    if (connStr) {
+      const exporter = new AzureMonitorTraceExporter({ connectionString: connStr })
+      const sdk = new NodeSDK({
+        serviceName: 'manage-a-workforce-ui',
+        spanProcessors: [new BatchSpanProcessor(exporter)],
+        instrumentations: [getNodeAutoInstrumentations()],
+      })
+      sdk.start()
+    }
+    global.otelStarted = true
   }
-  global.otelStarted = true
 }

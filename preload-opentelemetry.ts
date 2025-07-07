b375d41d208934c2f0b6ed45c8be3ada70b94405
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-07-07 16:40:12 +0100

    logic to check for kubernetes environment

--- preload-opentelemetry.ts
+++ preload-opentelemetry.ts
@@ -3,9 +3,11 @@ import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentation
 import { AzureMonitorTraceExporter } from '@azure/monitor-opentelemetry-exporter'
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base'
 
+process.env.OTEL_SERVICE_NAME = process.env.OTEL_SERVICE_NAME || 'manage-a-workforce-ui'
+
 if (process.env.NODE_ENV === 'production') {
   const key = process.env.APPINSIGHTS_INSTRUMENTATIONKEY
-  const connStr = `InstrumentationKey=${key};IngestionEndpoint=https://northeurope-0.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;`
+  const connStr = `InstrumentationKey=${key};IngestionEndpoint=https://northeurope-0.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;ApplicationId=0d5d6f09-1d07-47f5-9d3a-f9f473544e78;`
 
   // Prevent double initialization
   global.otelStarted = global.otelStarted || false

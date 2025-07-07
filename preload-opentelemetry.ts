ba78f99251d00c358bcb200abbac4bd0e6775321
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-07-07 15:22:44 +0100

    small refactor

--- preload-opentelemetry.ts
+++ preload-opentelemetry.ts
@@ -3,14 +3,14 @@ import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentation
 import { AzureMonitorTraceExporter } from '@azure/monitor-opentelemetry-exporter'
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base'
 
+const key = process.env.APPINSIGHTS_INSTRUMENTATIONKEY
+const connStr = `InstrumentationKey=${key};IngestionEndpoint=https://northeurope-0.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;`
+
 // Prevent double initialization
 global.otelStarted = global.otelStarted || false
 
 if (!global.otelStarted) {
-  const key = process.env.APPINSIGHTS_INSTRUMENTATIONKEY
-  const connStr = `InstrumentationKey=${key};IngestionEndpoint=https://northeurope-0.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;`
   if (connStr) {
-    // Setup Azure exporter and SDK
     const exporter = new AzureMonitorTraceExporter({ connectionString: connStr })
     const sdk = new NodeSDK({
       serviceName: 'manage-a-workforce-ui',

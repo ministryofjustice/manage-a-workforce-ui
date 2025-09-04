dd80c3d91f2e5edf01ac5befb7237064f2ba845b
Iain Chambers <iain.chambers@digital.justice.gov.uk>
2025-09-04 13:49:56 +0100

    Change to redis-stack-server and add cancellation of requests to debounce

--- assets/js/cache-instructions.js
+++ assets/js/cache-instructions.js
@@ -1,6 +1,8 @@
 const input = document.getElementById('instructions')
 const _csrf = document.querySelector('input[name="_csrf"]').value
 
+let controller
+
 instructions.addEventListener(
   'keyup',
   debounce(event => saveInput(event)),
@@ -19,8 +21,14 @@ function debounce(func, timeout = 300) {
 function saveInput(event) {
   const { crn, convictionNumber } = input.dataset
 
+  // If a request is still pending, cancel it to avoid race conditions
+  if (controller) controller.abort()
+
+  controller = new AbortController()
+
   fetch(`/notes/${crn}/${convictionNumber}`, {
     method: 'POST',
+    signal: controller.signal,
     credentials: 'same-origin',
     body: JSON.stringify({
       _csrf: _csrf,

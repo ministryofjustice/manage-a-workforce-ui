260e8486b7fc3ebb0aa2b936a62820ab2011ba75
Gary Reid <gary.reid@digital.justice.gov.uk>
2025-10-09 08:24:47 +0100

    WFP-3351 method renamed to isFeatureEnabled

--- server/services/featureFlagService.ts
+++ server/services/featureFlagService.ts
@@ -1,26 +1,24 @@
-import { VariantEvaluationResponse, BooleanEvaluationResponse } from '@flipt-io/flipt-client-js'
+import { BooleanEvaluationResponse } from '@flipt-io/flipt-client-js'
 import { createClient } from '../data/fliptClient'
 
 const fliptClient = createClient()
 
 export default class FeatureFlagService {
-  async getVariantFeatureFlag(key: string, context: Record<string, string>): Promise<boolean> {
-    const response = (await fliptClient).evaluateVariant({
-      entityId: key,
-      flagKey: key,
-      context,
-    }) as VariantEvaluationResponse
-
-    return response.match
+  async isFeatureEnabled(key: string): Promise<boolean> {
+    return this.isFeatureEnabledContext(key, null)
   }
 
-  async getFeatureFlag(key: string, context: Record<string, string>): Promise<boolean> {
-    const response = (await fliptClient).evaluateBoolean({
-      entityId: key,
-      flagKey: key,
-      context,
-    }) as BooleanEvaluationResponse
+  async isFeatureEnabledContext(key: string, context: Record<string, string>): Promise<boolean> {
+    try {
+      const response = (await fliptClient).evaluateBoolean({
+        entityId: key,
+        flagKey: key,
+        context,
+      }) as BooleanEvaluationResponse
 
-    return response.enabled
+      return response.enabled
+    } catch (error) {
+      return false
+    }
   }
 }

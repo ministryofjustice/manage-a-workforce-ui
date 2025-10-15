618059b9c209dc7da8459bad74b296e85d1d3896
katie.cawthorne <katie.cawthorne@digital.justice.gov.uk>
2025-10-15 09:27:57 +0100

    :triangular_flag_on_post: WFP-3328: added feature flag

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -14,7 +14,11 @@ type Error = {
   text: string
 }
 
-export default function nunjucksSetup(app: express.Express, path: pathModule.PlatformPath, services: Services): void {
+export default async function nunjucksSetup(
+  app: express.Express,
+  path: pathModule.PlatformPath,
+  services: Services,
+): Promise<void> {
   app.set('view engine', 'njk')
   app.locals.asset_path = '/assets/'
   app.locals.applicationName = 'Manage A Workforce Ui'
@@ -43,6 +47,8 @@ export default function nunjucksSetup(app: express.Express, path: pathModule.Pla
       express: app,
     },
   )
+
+  const featureFlag = await services.featureFlagService.isFeatureEnabled('reallocations', 'Reallocations')
 
   njkEnv.addFilter('initialiseName', initialiseName)
 
@@ -76,4 +82,5 @@ export default function nunjucksSetup(app: express.Express, path: pathModule.Pla
   njkEnv.addGlobal('tagManagerContainerId', config.analytics.tagManagerContainerId.trim())
   njkEnv.addGlobal('lastTechnicalUpdate', services.technicalUpdatesService.getLatestTechnicalUpdateHeading())
   njkEnv.addGlobal('instrumentationKey', config.instrumentationKey)
+  njkEnv.addGlobal('featureFlag', featureFlag)
 }

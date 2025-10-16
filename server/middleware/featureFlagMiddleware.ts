a6419d7edbca2a4f9129a9bc7d88e9fbb39cb973
katie.cawthorne <katie.cawthorne@digital.justice.gov.uk>
2025-10-16 21:59:40 +0100

    WFP-3328: fix linting

--- server/middleware/featureFlagMiddleware.ts
+++ server/middleware/featureFlagMiddleware.ts
@@ -3,10 +3,8 @@ import FeatureFlagService from '../services/featureFlagService'
 
 const featureFlagService = new FeatureFlagService()
 
-export async function featureFlagMiddleware(req: Request, res: Response, next: NextFunction) {
+export default async function featureFlagMiddleware(req: Request, res: Response, next: NextFunction) {
   const flag = await featureFlagService.getCachedFeatureflag('Reallocations', 'Reallocations')
   res.locals.featureFlag = flag
   next()
 }
-
-export default featureFlagMiddleware

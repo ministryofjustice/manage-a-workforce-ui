2766093c858d67b0b1ce17ec93aa144250b5682e
Katie Cawthorne <51707463+katie-cawthorne@users.noreply.github.com>
2025-10-20 15:30:07 +0100

    :recycle: WFP-3328: handle errors in feature flag middleware (#1232)

--- server/middleware/featureFlagMiddleware.ts
+++ server/middleware/featureFlagMiddleware.ts
@@ -1,10 +1,18 @@
 import { Request, Response, NextFunction } from 'express'
 import FeatureFlagService from '../services/featureFlagService'
+import logger from '../../logger'
 
 const featureFlagService = new FeatureFlagService()
 
-export default async function featureFlagMiddleware(req: Request, res: Response, next: NextFunction) {
-  const flag = await featureFlagService.getCachedFeatureflag('Reallocations', 'Reallocations')
-  res.locals.featureFlag = flag
-  next()
+export default function featureFlagMiddleware(flagKey, flagName) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const flag = await featureFlagService.getCachedFeatureflag(flagKey, flagName)
+      res.locals.featureFlag = flag
+    } catch (error) {
+      res.locals.featureFlag = null
+      logger.error(`Error fetching feature flag ${flagKey}`, error)
+    }
+    next()
+  }
 }

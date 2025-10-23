913901155c2ff3ee2adac47eeba6def4b6fc4026
katie.cawthorne <katie.cawthorne@digital.justice.gov.uk>
2025-10-23 11:55:30 +0100

    WFP-3383: fix integration test for footer

--- integration_tests/integration/home.spec.ts
+++ integration_tests/integration/home.spec.ts
@@ -44,12 +44,9 @@ context('Unallocated', () => {
     const allocateCasesByTeamPage = Page.verifyOnPage(AllocateCasesByTeamPage)
     allocateCasesByTeamPage
       .footer()
-      .should('contain', 'Accessibility statement')
-      .and('contain', 'Cookies')
-      .and('contain', 'Privacy')
-      .and('contain', "What's new")
-      .and('contain', 'Open Government Licence v3.0')
-      .and('contain', '© Crown copyright')
+      .should('contain', 'Accessibility')
+      .and('contain', 'Cookies policy')
+      .and('contain', 'Privacy policy')
   })
 
   it('technical updates banner visible on page', () => {

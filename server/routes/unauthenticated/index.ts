698e0bd32d2dc985c346d343e35508ba1e27fa19
katie.cawthorne <katie.cawthorne@digital.justice.gov.uk>
2025-10-23 11:41:51 +0100

    WFP-3383: Remove cookie and privacy policy pages as will be using probation frontend components pages

--- server/routes/unauthenticated/index.ts
+++ server/routes/unauthenticated/index.ts
@@ -2,15 +2,11 @@ import express, { Router } from 'express'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 
 import Accessibility from './accessibility'
-import Privacy from './privacy'
-import Cookies from './cookies'
 
 export default function UnauthenticatedRoutes(): Router {
   const router = express.Router()
 
   router.get('/accessibility-statement', asyncMiddleware(Accessibility()))
-  router.get('/privacy-notice', asyncMiddleware(Privacy()))
-  router.get('/cookie-policy', asyncMiddleware(Cookies()))
 
   return router
 }

2809a17d1c22af601271907aa63617f0f8abffdb
Iain Chambers <iain.chambers@digital.justice.gov.uk>
2025-12-10 17:46:44 +0000

    Add LAO and testing to reallocation CRN search result

--- integration_tests/mockApis/reallocations.ts
+++ integration_tests/mockApis/reallocations.ts
@@ -37,4 +37,78 @@ export default {
       },
     })
   },
+  stubExcludedCrnLookup: ({ crn }: { crn: string }): SuperAgentRequest => {
+    return stubForAllocation({
+      request: {
+        method: 'GET',
+        urlPattern: `/allocated/crn/${crn}`,
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: {
+          crn: 'A123456',
+          excluded: true,
+          apopExcluded: true,
+          name: {
+            forename: 'Jane',
+            middleName: '',
+            surname: 'Doe',
+            combinedName: 'Jane Doe',
+          },
+          dateOfBirth: '1958-05-25',
+          manager: {
+            code: 'T35T1NG',
+            name: {
+              forename: 'Unallocated',
+              middleName: '',
+              surname: 'Staff',
+              combinedName: 'Unallocated Staff',
+            },
+            teamCode: 'T35T1NG',
+            grade: null,
+            allocated: false,
+          },
+          hasActiveOrder: true,
+        },
+      },
+    })
+  },
+  stubLaoCrnLookup: ({ crn }: { crn: string }): SuperAgentRequest => {
+    return stubForAllocation({
+      request: {
+        method: 'GET',
+        urlPattern: `/allocated/crn/${crn}`,
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: {
+          crn: 'A123456',
+          excluded: true,
+          apopExcluded: false,
+          name: {
+            forename: 'Jane',
+            middleName: '',
+            surname: 'Doe',
+            combinedName: 'Jane Doe',
+          },
+          dateOfBirth: '1958-05-25',
+          manager: {
+            code: 'T35T1NG',
+            name: {
+              forename: 'Unallocated',
+              middleName: '',
+              surname: 'Staff',
+              combinedName: 'Unallocated Staff',
+            },
+            teamCode: 'T35T1NG',
+            grade: null,
+            allocated: false,
+          },
+          hasActiveOrder: true,
+        },
+      },
+    })
+  },
 }

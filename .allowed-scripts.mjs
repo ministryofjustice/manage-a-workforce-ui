8bcf5b063cb23f02c0d2e5da90974c454611bf69
Iain Chambers <iain.chambers@digital.justice.gov.uk>
2026-01-08 11:03:44 +0000

    Remove line that stops cypress installing from Dockerfile

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,7 +3,7 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
-    'node_modules/cypress@15.1.0': 'ALLOW',
+    'node_modules/cypress@15.1.0': 'FORBID',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     'node_modules/fsevents@2.3.3': 'FORBID',
     'node_modules/node@22.16.0': 'FORBID',

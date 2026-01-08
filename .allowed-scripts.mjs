073204cbf9aaa7cda5e76bde52c49f9ea1519f30
Iain Chambers <iain.chambers@digital.justice.gov.uk>
2026-01-08 10:52:47 +0000

    Update .allowed-scripts.mjs

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -6,7 +6,7 @@ export default configureAllowedScripts({
     'node_modules/cypress@15.1.0': 'FORBID',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     'node_modules/fsevents@2.3.3': 'FORBID',
-    'node_modules/node@22.21.1': 'ALLOW',
+    'node_modules/node@22.16.0': 'FORBID',
     'node_modules/unrs-resolver@1.11.1': 'FORBID',
   },
 })

eaa4c91be89b81088b121135a4e9dcfac773adda
Iain Chambers <iain.chambers@digital.justice.gov.uk>
2026-01-08 15:45:01 +0000

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,4 @@ import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
 export default hmppsConfig({
   extraIgnorePaths: ['assets'],
-  extraPathsAllowingDevDependencies: ['.allowed-scripts.mjs'],
 })

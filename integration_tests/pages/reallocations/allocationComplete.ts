d39884a5153757a1e79dc090cfbba06532018e17
Gary Reid <159784838+GaReid@users.noreply.github.com>
2026-01-26 13:40:59 +0000

    WFP-3360 confirmation page test (#1284)
    * WFP-3360 Confirmation page with details
    
    * WFP-3360 Confirmation page with details
    
    * WFP-3360 Add test
    
    * PR comments, remove duplication etc

---
+++ integration_tests/pages/reallocations/allocationComplete.ts
@@ -0,0 +1,17 @@
+import Page, { PageElement } from '../page'
+
+export default class AllocationCompletePage extends Page {
+  constructor() {
+    super('Case reallocated')
+  }
+
+  returnToUnallocatedLink = (): PageElement => cy.get('.govuk-link--no-visited-state')
+
+  panelBody = (): PageElement => cy.get('.govuk-panel__body')
+
+  panelTitle = (): PageElement => cy.get('.govuk-panel__title')
+
+  mediumHeading = (): PageElement => cy.get('.govuk-heading-m')
+
+  bulletedList = (): PageElement => cy.get('ul.govuk-list--bullet').children()
+}

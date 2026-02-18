346621d8a11e0a23f0cc51a9711627d3a5afcb49
Katie Cawthorne <51707463+katie-cawthorne@users.noreply.github.com>
2026-02-18 15:06:21 +0000

    WFP-3531: change event listener to use pagehide instead of beforeunload (#1306)
    * WFP-3531: change event listener to use pagehide instead of beforeunload
    
    * WFP-3531: make inGridRow false on review page

--- assets/js/notes-persist-confirm-allocation.js
+++ assets/js/notes-persist-confirm-allocation.js
@@ -1,13 +1,17 @@
-window.addEventListener('load', function () {
+window.addEventListener('load', () => {
   const FOUR_WEEKS_IN_SECONDS = 4 * 7 * 24 * 3600
-  var currentTimeInSeconds = Math.floor(Date.now() / 1000)
-  removeExpiredNotes(FOUR_WEEKS_IN_SECONDS, currentTimeInSeconds)
+  const currentTimeInSeconds = Math.floor(Date.now() / 1000)
+
+  removeExpiredNotes(FOUR_WEEKS_IN_SECONDS)
+
   const textArea = document.querySelector('[name="reallocationNotes"]')
   const reason = document.querySelector('[name="reason"]')
   const crn = textArea.getAttribute('data-crn')
   const newNotesItem = makeNotesKey(crn)
-  window.onbeforeunload = function () {
+
+  window.addEventListener('beforeunload', () => {
     saveNotes(textArea, reason, newNotesItem, currentTimeInSeconds)
-  }
+  })
+
   loadNotes(textArea, reason, newNotesItem)
 })

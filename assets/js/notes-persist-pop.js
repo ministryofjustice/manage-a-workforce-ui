df43e874405d1bbe4a60afb4de433ee5898e56b4
katie.cawthorne <katie.cawthorne@digital.justice.gov.uk>
2026-02-18 13:12:06 +0000

    WFP-3531: change event listener to use pagehide instead of beforeunload

--- assets/js/notes-persist-pop.js
+++ assets/js/notes-persist-pop.js
@@ -1,11 +1,13 @@
 window.addEventListener('load', function () {
-  var currentTimeInSeconds = Math.floor(Date.now() / 1000)
+  const currentTimeInSeconds = Math.floor(Date.now() / 1000)
   const textArea = document.querySelector('[name="reallocationNotes"]')
   const reason = document.querySelector('[name="reason"]')
   const crn = textArea.getAttribute('data-crn')
   const newNotesItem = makeNotesKey(crn)
-  window.onbeforeunload = function () {
+
+  window.addEventListener('pagehide', () => {
     saveNotes(textArea, reason, newNotesItem, currentTimeInSeconds)
-  }
+  })
+
   loadNotes(textArea, reason, newNotesItem)
 })

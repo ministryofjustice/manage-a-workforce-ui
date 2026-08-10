fb3f525b8341356be1dc6d35c9e1be60727b975d
Delia Savin <delia.savin@justice.gov.uk>
2026-08-10 09:49:53 +0100

    WFP-3627: Update integration test

--- integration_tests/pages/activeCases.ts
+++ integration_tests/pages/activeCases.ts
@@ -5,8 +5,6 @@ export default class ActiveCasesPage extends Page {
     super('Active cases')
   }
 
-  backLink = (): PageElement => cy.get('.govuk-back-link')
-
   secondaryText = (): PageElement => cy.get('.maw-secondary-text-col')
 
   heading = (): PageElement => cy.get('caption.govuk-heading-l')
